4dca3712a6ecc15ea7492ce40fee0407dbb6a607
Franz Puerto <puertofranz@gmail.com>
2025-03-20 17:24:17 +0100

    requests imports gefixt

--- static/Misty.py
+++ static/Misty.py
@@ -1,77 +1,94 @@
-import asyncio
-from pyodide.http import pyfetch
+import requests as rq
+from time import sleep
 
 class Misty:
     def __init__(self, ip):
         self.ip = ip
         self.api_url = f"http://{self.ip}/api/"
 
-    async def mistyResponse(self, command, parameters=None):
-        url = f"{self.api_url}{command}"
-        response = await pyfetch(url, method="POST", body=str(parameters) if parameters else None)
-        text = await response.text()
-        print(f"{url}\n{response.status}\n{text}")
+    def mistyResponse(self, command, parameters):
+        response = rq.post(f"{self.api_url}{command}", params=parameters)
+        print(f"{self.api_url}{command}\n{response}\n{response.text}")
         print(parameters)
 
-    async def halt(self):
-        await self.mistyResponse("halt")
+    # Stop all motor control
+    def halt(self):
+        self.mistyResponse("halt")
 
-    async def stop(self):
-        await self.mistyResponse("drive/stop")
+    # Stop Moving
+    def stop(self):
+        self.mistyResponse("drive/stop")
 
-    async def wait(self, tsec):
-        await asyncio.sleep(tsec)
+    def wait(self, tsec):
+        sleep(tsec)
 
-    async def setLEDHex(self, hex):
+    """ 
+    Expression
+    """
+    # Takes a hex value and sets the LED color to that Value
+    def setLEDHex(self, hex):
         hexint = int(hex, 16)
-        await self.setLEDRGB(hexint & 0xFF, (hexint >> 8) & 0xFF, (hexint >> 16) & 0xFF)
+        self.setLEDRGB(hexint & 0xFF, (hexint >> 8) & 0xFF, (hexint >> 16) & 0xFF)
 
-    async def setLEDRGB(self, r, g, b):
+    # Takes r, g and b as parameters and sets the LED accordingly
+    def setLEDRGB(self, r, g, b):
         parameters = {
             "red": r,
             "green": g,
             "blue": b
         }
-        await self.mistyResponse("led", parameters)
+        self.mistyResponse("led", parameters)
 
-    async def say(self, text):
-        parameters = {"Text": text}
-        await self.mistyResponse("tts/speak", parameters)
+    def say(self, text):
+        parameters = {
+            "Text": text
+        }
+        self.mistyResponse("tts/speak", parameters)
 
-    async def drive(self, linvel, angvel):
+    """
+    Movement Functions
+    """
+    # Drive in a Straight line
+    def drive(self, linvel, angvel):
         parameters = {
             "LinearVelocity": linvel,
             "AngularVelocity": angvel,
         }
-        await self.mistyResponse("drive", parameters)
+        self.mistyResponse("drive", parameters)
 
-    async def driveTimed(self, linvel, angvel, timems):
+    # Drive with all parameters
+    def driveTimed(self, linvel, angvel, timems):
         parameters = {
             "LinearVelocity": linvel,
             "AngularVelocity": angvel,
             "TimeMS": timems,
         }
-        await self.mistyResponse("drive/time", parameters)
+        self.mistyResponse("drive/time", parameters)
 
-    async def driveDistance(self, heading, distance, timems, reverse):
+    def driveDistance(self, heading, distance, timems, reverse):
         parameters = {
             "Heading": heading,
             "Distance": distance,
             "TimeMS": timems,
             "Reverse": reverse,
         }
-        await self.mistyResponse("drive/hdt", parameters)
+        self.mistyResponse("drive/hdt", parameters)
 
-    async def driveGrid(self):
-        for _ in range(4):
-            await self.driveTimed(50, 0, 647)
-            await asyncio.sleep(2)
-        await asyncio.sleep(3)
+    # def driveGrid(self):
+    #     self.driveTimed(50, 0, 1330)
+    #     sleep(3)
 
-    async def driveGridRev(self):
-        await self.driveTimed(-50, 0, 1330)
-        await asyncio.sleep(3)
+    def driveGrid(self):
+        for i in range(4):
+            self.driveTimed(50, 0, 647)
+            sleep(2)
+        sleep(3)
 
-    async def turnLeft(self):
-        await self.driveTimed(0, 100, 4435)
-        await asyncio.sleep(5)
+    def driveGridRev(self):
+        self.driveTimed(-50, 0, 1330)
+        sleep(3)
+
+    # Turn
+    def turnLeft(self):
+        self.driveTimed(0, 100, 4435)
+        sleep(5)
